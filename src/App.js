dbcc6c29578bb67da25494330951752def556f26
Sasi Kiran E <sasikiranessamaraju@gmail.com>
2022-06-21 19:49:05 +0530

    link to movie form.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
